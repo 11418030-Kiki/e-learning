265fd71f3b9f5e2e4c43d70eac7856f23f10b089
janmejay Singh <jvsingh.singh@gmail.com>
2018-04-13 18:53:23 +0530

    Font set

---
+++ app/src/main/java/com/e_learning/custom_view/MyButton.java
@@ -0,0 +1,37 @@
+package com.e_learning.custom_view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+
+
+@SuppressLint("AppCompatCustomView")
+public class MyButton extends Button {
+
+    public MyButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyButton(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(),
+                    "SF_SAN_FRANCISCO_UI_DISPLAY_REGULAR");
+            setTypeface(tf);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/e_learning/custom_view/MyEditText.java
@@ -0,0 +1,39 @@
+package com.e_learning.custom_view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+
+
+
+@SuppressLint("AppCompatCustomView")
+public class MyEditText extends EditText {
+
+
+    public MyEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MyEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(),
+                    "SF_SAN_FRANCISCO_UI_DISPLAY_REGULAR");
+            setTypeface(tf);
+        }
+    }
+
+}
